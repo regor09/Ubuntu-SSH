af1a642fa6a6bc792052da6b9fb7d9781a01977c
regor09 <154269943+regor09@users.noreply.github.com>
2023-12-26 17:24:22 +0700

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash#!/bin/bash
-git clone --depth=1 https://github.com/regor09/android_kernel_RMX2189R && cd android_kernel_RMX2189R
+git clone --depth=1 https://github.com/regor09/android_kernel_RMX2189R
+cd android_kernel_RMX2189R
 ./auto.sh
